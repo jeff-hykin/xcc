7085246d783278b617a6b70583844fe7d43463a4
Jeff Hykin <jeff.hykin@gmail.com>
2024-10-11 16:43:23 -0500

    cram helper in there

--- src/wcc/www/xcc.bundle.js
+++ src/wcc/www/xcc.bundle.js
@@ -18118,3 +18118,39 @@ export {
   DisWasm,
   WccRunner
 };
+
+
+export async function compileAndOrRun({args, sourceCode, compileAndDump=true}) {
+    var wccRunner = new WccRunner()
+    wccRunner.setConsoleOutFunction((text, _isError) => console.log(`[wccRunner.setConsoleOutFunction] error?: ${_isError}, text: ${text}`))
+    await wccRunner.setUp()
+    
+    const sourceName = "main.c"
+    const objFn = "main.o"
+    const compiledPath = "a.wasm"
+    const extraOptions = compileAndDump ? ["-c", "-o", objFn, "--import-module-name=env"] : undefined
+    await wccRunner.writeFile(sourceName, sourceCode)
+
+    const exitCode = await wccRunner.compile(sourceName, extraOptions)
+    if (exitCode !== 0) {
+        throw new Error(`Compile failed: ${exitCode}`)
+    }
+
+    if (compileAndDump) {
+        const compiledCode = await wccRunner.readFile(objFn)
+        const disWasm = new DisWasm(compiledCode.buffer)
+        let compiledWasm = ""
+        disWasm.setLogFunc((code)=>compiledWasm+=(code+"\n"))
+        // disWasm.setLogFunc((s) => Util.putTerminal(`${s}\n`))
+        disWasm.dump()
+        return compiledWasm
+    }
+
+    // Run
+    args = ["a.wasm", ...args]
+    const runExitCode = await wccRunner.runWasi(compiledPath, args)
+    if (runExitCode !== 0) {
+        throw new Error(`Run failed: ${runExitCode}`)
+    }
+    await wccRunner.clearTemporaries()
+}
