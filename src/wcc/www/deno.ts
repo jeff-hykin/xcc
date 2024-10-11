2e5e91ad7923f3c3f3f39ddc6267ebeb5a93c6d4
Jeff Hykin <jeff.hykin@gmail.com>
2024-10-11 16:23:18 -0500

    working in deno

---
+++ src/wcc/www/deno.ts
@@ -0,0 +1,102 @@
+import { DisWasm } from "./diswasm.ts"
+import { WccRunner } from "./wcc_runner.ts"
+
+var wccRunner = new WccRunner()
+wccRunner.setConsoleOutFunction((text, _isError) => console.log(`[wccRunner.setConsoleOutFunction] error?: ${_isError}, text: ${text}`))
+await wccRunner.setUp()
+ 
+async function run({args, sourceCode, compileAndDump=true}) {
+    const sourceName = "main.c"
+    const objFn = "main.o"
+    const compiledPath = "a.wasm"
+    const extraOptions = compileAndDump ? ["-c", "-o", objFn, "--import-module-name=env"] : undefined
+    await wccRunner.writeFile(sourceName, sourceCode)
+
+    const exitCode = await wccRunner.compile(sourceName, extraOptions)
+    if (exitCode !== 0) {
+        throw new Error(`Compile failed: ${exitCode}`)
+    }
+
+    if (compileAndDump) {
+        const compiledCode = await wccRunner.readFile(objFn)
+        const disWasm = new DisWasm(compiledCode.buffer)
+        let compiledWasm = ""
+        disWasm.setLogFunc((code)=>compiledWasm+=(code+"\n"))
+        // disWasm.setLogFunc((s) => Util.putTerminal(`${s}\n`))
+        disWasm.dump()
+        return compiledWasm
+    }
+
+    // Run
+    args = ["a.wasm", ...args]
+    const runExitCode = await wccRunner.runWasi(compiledPath, args)
+    if (runExitCode !== 0) {
+        throw new Error(`Run failed: ${runExitCode}`)
+    }
+    await wccRunner.clearTemporaries()
+}
+
+let compiledWasm = await run({
+    args: [],
+    sourceCode: `
+        #include <stdio.h>
+        
+        int fib(int n) {
+            if (n < 2)
+                return n;
+            else
+                return fib(n - 1) + fib(n - 2);
+        }
+        
+        int main() {
+            printf("%d\\n", fib(30));
+            return 0;
+        }
+    `,
+})
+console.log(`compiledWasm is:`,compiledWasm)
+Deno.exit()
+// const sourcePath = "/main.c"
+// await wccRunner.writeFile(sourcePath, `
+//     #include <stdio.h>
+
+//     int fib(int n) {
+//         if (n < 2)
+//             return n;
+//         else
+//             return fib(n - 1) + fib(n - 2);
+//     }
+
+//     int main() {
+//         printf("%d\\n", fib(30));
+//         return 0;
+//     }
+//     `)
+
+// let exitCode = await wccRunner.compile(sourcePath, [])
+
+// console.log(`exitCode is:`,exitCode)
+// Deno.exit()
+
+// try {
+//     console.log("here2")
+//     await wccRunner.writeFile("/main.c", `
+//     #include <stdio.h>
+
+//     int fib(int n) {
+//         if (n < 2)
+//             return n;
+//         else
+//             return fib(n - 1) + fib(n - 2);
+//     }
+
+//     int main() {
+//         printf("%d\\n", fib(30));
+//         return 0;
+//     }
+//     `)
+// } catch (error) {
+//     console.log("here")
+//     console.log(error)
+//     console.log(error.stack)
+// }
